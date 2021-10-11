91664e7395cc4357a9f6e09548a649f0b4da7920
alexacron <alexjorj@gmail.com>
2021-10-11 09:15:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# docker-php56-fpm-debian8
+
+This image is build for running a Symfony 2.3.0 on a PHP5.6 as FPM. 
+
+The PHP-FPM process is mapped on port 9001 and communicate with host's MySQL through tcp at host.docker.internal:3306
+
+
--- run-docker-php56.sh
+++ run-docker-php56.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-docker-compose up -d
+
+docker run -d --name php56-fpm -p 9001:9000 --mount type=bind,source="$PWD",target="$PWD" -v "$PWD/php-fpm.ini":/etc/php5/fpm/php.ini -v "$PWD/php-cli.ini":/etc/php5/cli/php.ini gesof/docker-php56-fpm-debian8
+
+#docker-compose up -d
